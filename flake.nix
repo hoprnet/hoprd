c23d6a67c19c6f6aca55e37a7e40da50075d8bbe
Ausiàs Armesto <ausias.armesto@hoprnet.org>
2026-07-28 10:58:01 +0200

    fix: Use fixed tag

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
     rust-overlay.url = "github:oxalica/rust-overlay/master";
     crane.url = "github:ipetkov/crane/v0.23.0";
-    nix-lib.url = "github:hoprnet/nix-lib/ausias/build-docker-aarch64";
+    nix-lib.url = "github:hoprnet/nix-lib/v1.2.1";
     foundry.url = "github:hoprnet/foundry.nix/tb/202505-add-xz";
     pre-commit.url = "github:cachix/git-hooks.nix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
