418be3ae353edcd4bd199b7518ec57e08687289c
Teebor-Choka <Teebor-Choka@users.noreply.github.com>
2026-09-04 11:25:37 +0000

    deploy: 957df6ff1020b11621f5af2f1b3dc558189a6705

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["hoprd","hoprd_api","hoprd_api_schema","hoprd_cfg"];
+//{"start":21,"fragment_lengths":[7,12,19,12]}
---
+++ trait.impl/axum_core/response/into_response/trait.IntoResponse.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["hoprd_api",[["impl IntoResponse for <a class=\"enum\" href=\"hoprd_api/enum.ApiErrorStatus.html\" title=\"enum hoprd_api::ApiErrorStatus\">ApiErrorStatus</a>",0],["impl IntoResponse for <a class=\"struct\" href=\"hoprd_api/struct.ApiError.html\" title=\"struct hoprd_api::ApiError\">ApiError</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[303]}
